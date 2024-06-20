d889b19a862b91991da377107652760bbde2722d
LucasKniska <lucaskniska1@gmail.com>
2024-06-20 00:38:02 -0400

    Lab 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card1 from './Card1';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>CS 230L</h1>
       <h2>Section - 702</h2>
       <p>WVU ID: 800403404</p>
       <p>Hi I am Lucas</p>
+
+
+      <Card1 num="1"/>
+      <Card1 num="2"/>
+      <Card1 num="3"/>
+
     </div>
   );
 }
